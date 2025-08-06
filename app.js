a6ab18b1e7d162b3935b51528e1a1e5547eda8ed
Lucia Gallardo <luciagallardoherrera@gmail.com>
2025-08-06 20:10:09 -0300

    configuracion inicial

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 import express, { urlencoded } from "express";
 import dotenv from "dotenv";
+import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
-// Importar rutas
-import roomsRouter from "./src/routes/rooms.js";
 
 dotenv.config();
 
@@ -15,27 +13,47 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// --- Middlewares ---
 const corsOptions = {
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+
   credentials: true,
+
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
+
+  allowedHeaders: ["Content-type", "Authorization", "Set-Cookie"],
+
   exposedHeaders: ["Set-Cookie"],
 };
 app.use(cors(corsOptions));
+
 app.use(express.json());
+
 app.use(urlencoded({ extended: true }));
+
 app.use(morgan("dev"));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // --- Rutas ---
 app.use("/api/rooms", roomsRouter);
 
-// Ruta de prueba
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
+});
+
+const routeFiles = fs.readdirSync("./src/routes");
+
+routeFiles.forEach((file) => {
+  import(`./src/routes/${file}`)
+    .then((route) => {
+      app.use("/api/v1", route.default);
+    })
+    .catch((err) => {
+      console.error(`Error al cargar la ruta ${file}:`, err);
+    });
 });
 
 export default app;
