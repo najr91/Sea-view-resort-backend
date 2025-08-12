fa75614f05706f35d7c70d91397be70b6406af76
Lucia Gallardo <luciagallardoherrera@gmail.com>
2025-08-12 13:05:25 -0300

    middleware autenticacion token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,4 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const authRequired = (req, res, next) => {
   const { token } = req.cookies;
+
+  if (!token) return res.status(401).json({ message: "Token no recibido" });
+
+  jwt.verify(token, process.env.SECRET_KEY_TOKEN, (err, userDecoded) => {
+    if (err) return res.status(403).json({ message: "Token invalido" });
+
+    req.user = userDecoded;
+
+    return next();
+  });
 };
