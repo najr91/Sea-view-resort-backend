b1dbc707471dc3a8e0685535bf572bc56977638c
Lucia Gallardo <luciagallardoherrera@gmail.com>
2025-08-14 18:23:13 -0300

    limpiando configuracion de inicio para merge

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config({ quiet: true });
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
@@ -34,7 +34,7 @@ app.use(morgan("dev"));
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(_dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const routeFiles = fs.readdirSync("./src/routes");
 
--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Conexión exitosa a MongoDB");
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error);
