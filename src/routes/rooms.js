1c700e7a380d032aa94f3b4b030330e2921f2f59
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-08-16 19:10:53 -0300

    inicializo configuracion de multer y modifico roomController.js para poder crear y eliminar categorias de habitaciones

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,15 +1,36 @@
 import express from "express";
-import { getRooms, createRoom, updateRoom, deleteRoom } from "../controllers/roomController.js";
+import multer from "multer";
+import { 
+  getRooms, 
+  createRoom, 
+  updateRoom, 
+  deleteRoom, 
+  addPhotoToRoom, 
+  deletePhotoFromRoom 
+} from "../controllers/roomController.js";
 import { protect, isAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Ruta pública: cualquiera puede ver habitaciones
-router.get("/", getRooms);
 
-// Rutas protegidas: solo admin
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/rooms/"); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // nombre único
+  },
+});
+const upload = multer({ storage });
+
+
+router.get("/", getRooms); 
 router.post("/", protect, isAdmin, createRoom);
 router.put("/:id", protect, isAdmin, updateRoom);
 router.delete("/:id", protect, isAdmin, deleteRoom);
+
+
+router.post("/:id/photos", protect, isAdmin, upload.single("photo"), addPhotoToRoom);
+router.delete("/:id/photos/:photoName", protect, isAdmin, deletePhotoFromRoom);
 
 export default router;
