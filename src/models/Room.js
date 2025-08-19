46d2e4e4ef441f960467e6622455176508dce35c
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-08-19 17:15:56 -0300

    reestructuracion de model de room para solucionar conflicto con frontend

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const roomSchema = new mongoose.Schema({
-  type: { type: String, required: true },
+  name: { type: String, required: true },
   price: { type: Number, required: true },
-  available: { type: Boolean, default: true },
-  photos: [{ type: String }]
+  description: { type: String, required: true },
+  images: [{ type: String }]
 });
 
 export default mongoose.model("Room", roomSchema);
